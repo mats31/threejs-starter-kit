2cb1ee7a74a6f0f1a572bf69816b97d3c26596ec
Mathis Biabiany <mathisbiabiany@Mathiss-MacBook-Pro.local>
2018-11-15 22:09:45 +0900

    Update threejs starter kit

--- app/index.js
+++ app/index.js
@@ -1,30 +1,27 @@
-import Webgl from './Webgl';
-import raf from 'raf';
-import dat from 'dat-gui';
-import 'gsap';
-
-let webgl;
-let gui;
+import Webgl from './Webgl'
+import raf from 'raf'
+import dat from 'dat-gui'
+import 'gsap'
 
 // webgl settings
-webgl = new Webgl( window.innerWidth, window.innerHeight );
-document.body.appendChild( webgl.renderer.domElement );
+const webgl = new Webgl( window.innerWidth, window.innerHeight )
+document.body.appendChild( webgl.renderer.domElement )
 
 // GUI settings
-gui = new dat.GUI();
+const gui = window.gui = new dat.GUI()
 
 function resizeHandler() {
-  webgl.resize( window.innerWidth, window.innerHeight );
+  webgl.resize( window.innerWidth, window.innerHeight )
 }
 
 function animate() {
-  raf( animate );
+  raf( animate )
 
-  webgl.render();
+  webgl.render()
 }
 
 // handle resize
-window.addEventListener( 'resize', resizeHandler );
+window.addEventListener( 'resize', resizeHandler )
 
 // let's play !
-animate();
+animate()
