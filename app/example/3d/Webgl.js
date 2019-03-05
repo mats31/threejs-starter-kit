a71a276d05a985a52eb028e4c80d43561c1853a2
Mathis Biabiany <mathis.biabiany@gmail.com>
2019-03-06 00:56:48 +0100

    Update webgl class example

--- app/example/3d/Webgl.js
+++ app/example/3d/Webgl.js
@@ -1,40 +1,56 @@
-import { autobind } from 'core-decorators';
+import { autobind } from 'core-decorators'
 
 export default class WebGL {
 
   // Setup ---------------------------------------------------------------------
 
-  constructor() {
-    this._setupWebGL(window.innerWidth, window.innerHeight);
+  constructor(options) {
+    this._width = window.innerWidth
+    this._height = window.innerHeight
+
+    this._setupWebGL(this._width, this._height)
   }
 
   _setupWebGL(width, height) {
-    this._scene = new THREE.Scene();
+    this._scene = new THREE.Scene()
 
-    this._camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
-    this._camera.position.z = 100;
+    this._camera = new THREE.PerspectiveCamera(45, this._width / this._height, 0.1, 1000)
+    this._camera.position.z = 100
 
-    this._renderer = new THREE.WebGLRenderer();
-    this._renderer.setSize( width, height );
-    this._renderer.setClearColor( 0x000000 );
+    // this._camera = new THREE.OrthographicCamera(this._width / - 2, this._width / 2, this._height / 2, this._height / - 2, 0, 100);
+    // this._scene.add(this._camera);
+
+    this._renderer = new THREE.WebGLRenderer()
+    this._renderer.setSize(this._width, this._height)
+    this._renderer.setClearColor(0x000000)
   }
 
   _setupEvents() {
-    Signals.onResize.add(this._onResize);
+    Signals.onResize.add(this._onResize)
   }
 
   // Events --------------------------------------------------------------------
 
   @autobind
   _onResize(vw, vh) {
-    this.resize(vw, vh);
+    this.resize(vw, vh)
   }
 
   resize(vw, vh) {
-    this._camera.aspect = vw / vh;
-    this._camera.updateProjectionMatrix();
+    this._width = window.innerWidth
+    this._height = window.innerHeight
 
-    this._renderer.setSize( vw, vh );
+    this._camera.aspect = this._width / this._height
+    this._camera.updateProjectionMatrix()
+
+    // Orthographic camera
+    // this._camera.left = this._width / - 2;
+    // this._camera.right = this._width / 2;
+    // this._camera.top = this._height / 2;
+    // this._camera.bottom = this._height / - 2;
+    // this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(this._width, this._height)
   }
 
 }
